34368a5741bee74f4c92668a9deef2a82f02785d
GeorgeOrzanescu <george.asro@gmail.com>
2022-02-20 12:01:24 +0200

    Module 6 done

---
+++ 06. Advanced Types/src/advanced_types.ts
@@ -0,0 +1,119 @@
+// Code goes here!
+
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+// using intersection type
+type ElevetedEmployee = Admin & Employee;
+
+const e1: ElevetedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+console.log(e1.privileges);
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type universal = Combinable & Numeric;
+
+let result: universal = 2;
+
+// type guard  helps with union types
+
+function add(a: Combinable, b: Combinable) {
+  // this is a type guard
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name " + emp.name);
+  if ("privileges" in emp) {
+    console.log("privilages " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("start date " + emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(100);
+  }
+}
+
+// discriminated union
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  switch (animal.type) {
+    case "bird":
+      console.log("Flying with speed " + animal.flyingSpeed);
+      break;
+    case "horse":
+      console.log("Moving with speed " + animal.runningSpeed);
+      break;
+  }
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+//type casting
+
+// first option
+// const userInput = <HTMLInputElement>document.getElementById("user-input");
+// second option
+const userInput = document.getElementById("user-input")! as HTMLInputElement;
+
+userInput.value = "Hello World";
+
+
--- 06. Advanced Types/src/app.ts
+++ 06. Advanced Types/src/app.ts
@@ -1,35 +1,19 @@
-// Code goes here!
+interface ErrorContainer {
+  [prop: string]: string;
+}
 
-type Admin = {
-  name: string;
-  privileges: string[];
+const errorBag: ErrorContainer = {
+  email: "not a valid email",
+  username: "should be at least 3 characters",
 };
 
-type Employee = {
-  name: string;
-  startDate: Date;
-};
-// using intersection type
-type ElevetedEmployee = Admin & Employee;
+console.log(errorBag.email, errorBag.username);
 
-const e1: ElevetedEmployee = {
-  name: "Max",
-  privileges: ["create-server"],
-  startDate: new Date(),
-};
+// FUNCTION OVERLOADING
 
-console.log(e1.privileges);
-
-type Combinable = string | number;
-type Numeric = number | boolean;
-
-type universal = Combinable & Numeric;
-
-let result: universal = 2;
-
-// type guard  helps with union types
-
-function add(a: Combinable, b: Combinable) {
+function add2(a: number, b: number): number;
+function add2(a: string, b: string): string;
+function add2(a: Combinable, b: Combinable) {
   // this is a type guard
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
@@ -37,81 +21,29 @@ function add(a: Combinable, b: Combinable) {
   return a + b;
 }
 
-type UnknownEmployee = Employee | Admin;
+// const result2 = add2("George", "Burdell") as string; // type casting
 
-function printEmployeeInfo(emp: UnknownEmployee) {
-  console.log("Name " + emp.name);
-  if ("privileges" in emp) {
-    console.log("privilages " + emp.privileges);
-  }
-  if ("startDate" in emp) {
-    console.log("start date " + emp.startDate);
-  }
-}
+const result_num = add2(2, 2);
+const result_string = add2("George", "Burdell");
 
-printEmployeeInfo(e1);
+result_string.split("");
 
-class Car {
-  drive() {
-    console.log("Driving...");
-  }
-}
+// OPTIONAL CHAINING
 
-class Truck {
-  drive() {
-    console.log("Driving a truck...");
-  }
+const fetchedUserData = {
+  id: "u1",
+  name: "Max",
+  job: {
+    title: "CEO",
+    description: "My own company",
+  },
+};
 
-  loadCargo(amount: number) {
-    console.log("Loading cargo " + amount);
-  }
-}
+console.log(fetchedUserData?.job?.title);
 
-type Vehicle = Car | Truck;
+// NULLISH COALESCING
+const userInputData = null;
 
-const v1 = new Car();
-const v2 = new Truck();
-
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive();
-
-  if (vehicle instanceof Truck) {
-    vehicle.loadCargo(100);
-  }
-}
-
-// discriminated union
-
-interface Bird {
-  type: "bird";
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: "horse";
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimal(animal: Animal) {
-  switch (animal.type) {
-    case "bird":
-      console.log("Flying with speed " + animal.flyingSpeed);
-      break;
-    case "horse":
-      console.log("Moving with speed " + animal.runningSpeed);
-      break;
-  }
-}
-
-moveAnimal({ type: "bird", flyingSpeed: 10 });
-
-//type casting
-
-// first option
-// const userInput = <HTMLInputElement>document.getElementById("user-input");
-// second option
-const userInput = document.getElementById("user-input")! as HTMLInputElement;
-
-userInput.value = "Hello World";
+const storedData = userInputData ?? "DEFAULT";
+// if userInputData is null, storedData will be "DEFAULT"
+// ?? is the nullish coalescing operator
